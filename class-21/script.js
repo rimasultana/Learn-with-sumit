0728356d9030af6a2522485f725f71dd9fb24d96
rimasultana <rimasultana20225@gmail.com>
2024-01-20 21:34:04 +0600

    arrat

---
+++ class-21/script.js
@@ -0,0 +1,11 @@
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// // blank array
+
+// const cars = [];
+//  car [0] = ["Saab"] ;
+//  car [1] = ["Volvo"]
+//  car [2] = ["BMW"]
+
+const cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars;
