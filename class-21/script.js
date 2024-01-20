a6c72e9cb8e3f1348641eae6d3d4de0b0cdd90d4
rimasultana <rimasultana20225@gmail.com>
2024-01-20 22:18:20 +0600

    js

--- class-21/script.js
+++ class-21/script.js
@@ -6,17 +6,23 @@
 //  car [1] = ["Volvo"]
 //  car [2] = ["BMW"]
 
-const cars = ["Saab", "Volvo", "BMW"];
-console.log(typeof cars )
-document.getElementById("demo").innerHTML = cars;
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(typeof cars )
+// document.getElementById("demo").innerHTML = cars;
 
-// Array
-const person = ["jhon", "Dho", "46"]
-// object
-const person = {firstName:"jhon", lastName:"Dho", age:46}
+// // Array
+// const person = ["jhon", "Dho", "46"]
+// // object
+// const person = {firstName:"jhon", lastName:"Dho", age:46}
 
-// Array is super power element
+// // Array is super power element
 
-myArray [0] = Date.now;
-myArray [1] = myFuntion;
-myArray [2] = mycars;
+// myArray [0] = Date.now;
+// myArray [1] = myFuntion;
+// myArray [2] = mycars;
+
+const fruits = ["banana", "Apple", "orange","painaple"]
+for ( i = 0; i <fruits.length;i++);{
+    console.log(i);
+    console.log(fruits[i]);
+}
