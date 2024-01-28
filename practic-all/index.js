ce9ac02eef4f4a17ccad694dfdfcb23533c48ed9
rimasultana <rimasultana20225@gmail.com>
2024-01-28 21:26:34 +0600

    number

--- practic-all/index.js
+++ practic-all/index.js
@@ -1,12 +1,3 @@
-// let text = "Please locate where 'locate' occurs!";
-// let index = text.indexOf("locate");
-// let text = "Please locate where your location";
-// console.log(text.search("Please"));
-// let text = "The rain in SPAIN stays mainly in the plain";
-// text.match("ain");
-// console.log(text.match("ain"));
-let str = `Banhladesh`;
-let a = 17;
-let b = 2;
-let sentence = `I love ${country} is big ${a+b} country`;
-console.log(sentence);
+let x = 3.085785;
+let y = 0.546;
+console.log( x + y );
