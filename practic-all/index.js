4fd618384e83823b494f01e7757ae87b2c78e267
rimasultana <rimasultana20225@gmail.com>
2024-01-28 21:14:48 +0600

    ajaira

--- practic-all/index.js
+++ practic-all/index.js
@@ -2,6 +2,9 @@
 // let index = text.indexOf("locate");
 // let text = "Please locate where your location";
 // console.log(text.search("Please"));
-let text = "The rain in SPAIN stays mainly in the plain";
-text.match("ain");
-console.log(text.match("ain"));
+// let text = "The rain in SPAIN stays mainly in the plain";
+// text.match("ain");
+// console.log(text.match("ain"));
+let str = `Banhladesh`;
+let sentence = `I love ${country} is big country`;
+console.log("sentence");
