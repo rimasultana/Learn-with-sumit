31932172a73896e1cf0e521c27ee9671213cad42
rimasultana <rimasultana20225@gmail.com>
2024-01-28 21:04:35 +0600

    match

--- practic-all/index.js
+++ practic-all/index.js
@@ -1,4 +1,7 @@
 // let text = "Please locate where 'locate' occurs!";
 // let index = text.indexOf("locate");
-let text = "Please locate where your location";
-console.log(text.search("Please"));
+// let text = "Please locate where your location";
+// console.log(text.search("Please"));
+let text = "The rain in SPAIN stays mainly in the plain";
+text.match("ain");
+console.log(text.match("ain"));
