6e36c3cb0c8bb33938bfdb9687ee71d0d42190ae
rimasultana <rimasultana20225@gmail.com>
2024-02-04 20:38:55 +0600

    sort

--- practic-all/index.js
+++ practic-all/index.js
@@ -1,14 +1,2 @@
-// // ludu khalai 1-6 porjonto radmly 
-
-// function getRandomNumber(min,max){
-
-// return Math.floor(Math.random() * (max - min +1)) + min;
-
-// }
-// console.log(getRandomNumber(1,6));
-
-
-// kii bave amra amder class er sokolar name alfabatic sajate pari
-
-const students = ["Sumit","Saad","Rima","Akash"];
-console.log(student.sort());
+const students = ["RIma","Mobasshir","Rupa","Akash"];
+console.log(students.sort());
