0322b0777a75b63b68b900427bc1acd5fde56f21
rimasultana <rimasultana20225@gmail.com>
2024-02-04 22:30:15 +0600

    return count

--- practic-all/index.js
+++ practic-all/index.js
@@ -1,10 +1,13 @@
-function isLeapYear(year){
-    if((year % 400 === 0) || ((year % 4 ===0) 
-    &&(year % 100 !== 0))){
-console.log(`${year} is  a leap year!`);
+const vowels = ["a","e","i","o","u","A","E","I","O","U"];
+
+function constVowels(sentence){
+    let count = 0;
+    const letters = Array.from(sentence);
+    letters.forEach(function(value){
+        if(vowels.forEach(value)){
+            count++;
+        }
+    });
+    return count;
 }
-else{
-    console.log(`${year} is not a leap year`)
-}
-}
-isLeapYear(2028);
+console.log(countVowels("I love Bangladesh"));
