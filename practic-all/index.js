204ed5b1987cd8b0800ebb3ee29949cad9501ace
rimasultana <rimasultana20225@gmail.com>
2024-02-04 20:51:46 +0600

    sort return

--- practic-all/index.js
+++ practic-all/index.js
@@ -1,2 +1,5 @@
-const students = ["RIma","Mobasshir","Rupa","Akash"];
-console.log(students.sort());
+const roll_number = [5,3,9,2,3,1,4,7,8];
+console.log(roll_number.sort(function
+    (a,b) {
+        return a - b;
+    }))
