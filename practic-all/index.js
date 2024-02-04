814bd9463b82443a91b2efce10ae8a3336738f3d
rimasultana <rimasultana20225@gmail.com>
2024-02-04 20:58:06 +0600

    new code added

--- practic-all/index.js
+++ practic-all/index.js
@@ -1,5 +1,5 @@
-const roll_number = [5,3,9,2,3,1,4,7,8];
-console.log(roll_number.sort(function
-    (a,b) {
-        return a - b;
-    }))
+const roll_number = [2,1,3,5,6,7,4,8];
+console.log(roll_number.sort(function(a,b)
+{
+    return b - a;
+}))
